87597eb15c22da6ed3b329a16b8fe38e83677399
Raunak <rrb3493@gmail.com>
2024-06-01 18:38:22 +1000

    tictactoe-react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+//brings all the pieces together and injects the final product into index.html in the public folder.
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
